c5e42d967d2f97798cc472f2ea39cf42d62f4e17
pjourdan <pauljourdanbaez@gmail.com>
2023-06-27 16:28:16 -0400

    Areglo de script de instanciacion de tabla

--- docker-compose-img/mysql/sql/init.sql
+++ docker-compose-img/mysql/sql/init.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS mensajes {
+CREATE TABLE IF NOT EXISTS mensajes (
     id_mensaje BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     mensaje VARCHAR(280),
     autor_mensaje VARCHAR(50),
     fecha_mensaje TIMESTAMP
-};
+);
